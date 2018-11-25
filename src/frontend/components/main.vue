2a49553003a54f845a19a8a2fa096266fa2865fa
Adam Dominik <adam.dominik89@gmail.com>
2018-11-25 13:51:38 +0100

    login form

--- src/frontend/components/main.vue
+++ src/frontend/components/main.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="o-data-container">
-    Zaloguj sie
+    <div class="o-login-container">
+      Zaloguj sie
+
+      <div class="m-single-container">
+        <div class="m-user">
+          <label class="a-label f-login">Login:
+          <input type="text"/></label>
+        </div>
+      </div>
+      <div class="m-single-container">
+        <div class="m-password">
+          <label>Hasło:
+          <input type="text"/>
+          </label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data: () => ({
-  }),
+  name: 'Main',
+  data: () => ({}),
+  computed: {},
+
+  methods: {
+
+  },
+
 }
 </script>
 
