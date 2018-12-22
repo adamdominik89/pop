c93bb7974b5ba80d1e03bb32e89e166e87a7e974
Adam Dominik <adam.dominik89@gmail.com>
2018-12-22 17:37:43 +0100

    tabelka bazowa bez uzycia stora

--- src/frontend/components/pages/stock/actual_stock.vue
+++ src/frontend/components/pages/stock/actual_stock.vue
@@ -1,17 +1,53 @@
 <template>
   <div class="o-data-container">
     <page-data title="Stany towarów">
-      tabelka z aktualnie posiadanymi towarami
+      <vue-good-table
+        :columns="columns"
+        :rows="rows"
+        :groupOptions="{enabled: true}"
+      ></vue-good-table>
     </page-data>
   </div>
 </template>
 
 <script>
 import PageData from '../../templates/page-data'
+import { VueGoodTable } from 'vue-good-table'
 export default {
   name: 'actual-stock',
-  components: {PageData},
-  data: () => ({})
+  components: {PageData, VueGoodTable},
+  data: () => ({
+    columns: [ {
+      label: 'Numer partii',
+      field: 'part_number'
+    },
+    {
+      label: 'Ilość w [kg]',
+      field: 'quantity'
+    },
+    {
+      label: 'Data przydatności do spożycia',
+      field: 'best_before'
+    },
+    {
+      label: 'Producent',
+      field: 'producer'
+    }
+    ],
+    rows: [{
+      mode: 'span',
+      label: 'Mąka pszenna typ 500',
+      html: false,
+      children: [
+        {
+          part_number: '100/2019', quantity: 550, best_before: '10-12-2018', producer: 'Mlyn tadeusz Rybak'
+        },
+        {
+          part_number: '21/01/2018', quantity: 4000, best_before: '10-12-2020', producer: 'mlyn xyz'
+        }
+      ]
+    }]
+  })
 }
 </script>
 
