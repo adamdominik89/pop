3c44c9ce760339c546d58f667c422f283cd78e69
Adam Dominik <adam.dominik89@gmail.com>
2018-12-24 12:36:56 +0100

    radio buttons in towary przyjecie towaru

--- src/frontend/components/pages/stock/recive_goods.vue
+++ src/frontend/components/pages/stock/recive_goods.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="o-data-container">
-    <page-data title="Przyjecie towaru">
-      Przyjęcie :
+    <page-table title="Przyjecie towaru">
+
+      <div class="a-input-container f-data">
+        <label class="a-input-label"> Przyjęcie : </label>
+        <div class="a-form-container f-fiscalization">
+          <template v-for="(type, key) in recive_type"
+          >
+            <div class="a-radio-single"
+                 :key="key">
+              <label>
+                <input v-model="value_from_v_model" :value="type.value" type="radio" />
+                {{ type.label }}
+              </label>
+            </div>
+          </template>
+        </div>
+      </div>
 
       select ilosc numer partii jeszce jakies info
-    </page-data>
+    </page-table>
   </div>
 </template>
 
 <script>
-import PageData from '../../templates/page-data'
+
+import PageTable from '../../templates/page-table'
 
 export default {
   name: 'Recive-Goods',
-  components: {PageData},
+  components: {PageTable},
   data: () => ({
+    value_from_v_model: 0,
+    recive_type: [
+      {
+        label: 'Istniejącego towaru',
+        value: 0
+      },
+      {
+        label: 'Nowego towaru',
+        value: 1
+      }
+    ]
   }),
   methods: {
   }
