98bc1434e2f63596fceb95afe2916cc860c308e3
Adam Dominik <adam.dominik89@gmail.com>
2018-12-29 13:15:54 +0100

    select wraz z inputem do wyliczenia ilości produkcyjnych

--- src/frontend/components/pages/production/plan.vue
+++ src/frontend/components/pages/production/plan.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="o-data-container">
-    <page-data title="Planowanie produkcji">
-      zaplanowanie produkcji
-      lista linkow do receptur / przenosza do okna gdzie pojawia sie nazwa oraz input ile chcesz wyprodukowac ? /
-      potwierdzasz przyciskiem wyswietla sie tabelka z receptura (nazwy oraz ilosci jakie posiadamy na stanie magazynowym oraz
-      ilosci jakie potrzebujemy domowic aby wyprodukowac porzadana ilość
-    </page-data>
+    <page-table title="Planowanie produkcji">
+      <div>
+        <label class="a-select-label">
+        Wybierz produkt:
+          <a-select></a-select></label>
+      </div>
+      <div>
+        <label class="a-select-label">
+          Wprowadz ilość w kg do produkcji:<input class="a-input-recive-goods"/></label>
+      </div>
+      <button @click="show_table">Wylicz!</button>
+      <div v-if="count === true">
+        nazwa produktu
+        tabelka
+        półprodukty, jakie ilości posiadamy na stanie magazynowym oraz ilości jakie potrzeba domówić
+      </div>
+    </page-table>
 
   </div>
 </template>
 
 <script>
-import PageData from '../../templates/page-data'
+import PageTable from '../../templates/page-table'
+import ASelect from '../../atoms/select'
+
 export default {
   name: 'PProduction-plan',
-  components: {PageData},
+  components: {ASelect, PageTable},
   data: () => ({
-
-  })
+    count: false
+  }),
+  methods: {
+    show_table () {
+      this.count = true
+    }
+  }
 }
 </script>
 
