035c3a25eee64637e08871aba2791857d59c122d
Adam Dominik <adam.dominik89@gmail.com>
2018-12-29 13:25:36 +0100

    tytuł podany nad tabelką

--- src/frontend/components/pages/production/plan.vue
+++ src/frontend/components/pages/production/plan.vue
@@ -4,7 +4,7 @@
       <div>
         <label class="a-select-label">
         Wybierz produkt:
-          <a-select></a-select></label>
+          <a-select :options="get_recipes" v-model="product_name"></a-select></label>
       </div>
       <div>
         <label class="a-select-label">
@@ -12,7 +12,10 @@
       </div>
       <button @click="show_table">Wylicz!</button>
       <div v-if="count === true">
-        nazwa produktu
+        <div>
+        {{product_name}}
+        </div>
+        <vue-good-table></vue-good-table>
         tabelka
         półprodukty, jakie ilości posiadamy na stanie magazynowym oraz ilości jakie potrzeba domówić
       </div>
@@ -24,13 +27,32 @@
 <script>
 import PageTable from '../../templates/page-table'
 import ASelect from '../../atoms/select'
+import {mapGetters} from 'vuex'
+import { VueGoodTable } from 'vue-good-table'
 
 export default {
   name: 'PProduction-plan',
-  components: {ASelect, PageTable},
+  components: {ASelect, PageTable, VueGoodTable},
   data: () => ({
-    count: false
+    count: false,
+    product_name: '',
   }),
+  computed: {
+    ...mapGetters('production', ['get_recipe_links']),
+    get_recipes () {
+      let arrayofoptions = []
+      let arrayfromstore = this.get_recipe_links
+      for (let i = 0 ; i < arrayfromstore.length; i++){
+        let obj = {
+          label: arrayfromstore[i].label,
+          value: arrayfromstore[i].label
+        }
+        arrayofoptions.push(obj)
+      }
+
+      return arrayofoptions
+    }
+  },
   methods: {
     show_table () {
       this.count = true
